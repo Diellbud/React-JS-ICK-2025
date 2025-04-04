c49aa95ca22bed74a14475198ee6ddf9102510b8
Diell <budimadiell@gmail.com>
2025-04-04 20:20:02 +0200

    Ora-1

---
+++ src/components/reviewPage/Review.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+function Review() {
+    return <>
+            <input type="radio" name="reviewRadio"/>
+    </>
+}
+
+export default Review
---
+++ src/components/reviewPage/WriteReview.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function WriteReview() {
+  return (
+    <>
+        <input type="text" placeholder="Write a Review" />
+        <button>Send</button>
+    </>
+  );
+}
+
+export default WriteReview;
